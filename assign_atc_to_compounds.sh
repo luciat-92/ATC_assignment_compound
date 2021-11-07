e0d4ead11df8ceb9d948fbf8022801e65f402830
Lucia Trastulla <lucia.trastulla@fht.org>
2021-11-07 11:40:17 +0100

    complete scripts call

--- assign_atc_to_compounds.sh
+++ assign_atc_to_compounds.sh
@@ -2,7 +2,7 @@
 
 #SBATCH --job-name=atc
 #SBATCH --partition=cpuq
-#SBATCH --time=62:00:00
+#SBATCH --time=7-0
 #SBATCH --nodes=1
 #SBATCH --output=atc_%j.out
 #SBATCH --error=atc_%j.err
@@ -15,16 +15,22 @@ conda activate py38
 
 cd /home/lucia.trastulla/workspace/ATC_assignment_compound/
 
-# extract ATC compounds chemblIDs
-python src/extract_chembl_id.py --compound_list_csv data/covid_compound_names.csv --output_file_name data/compounds_covid_chemblid.json
 # extract merged compounds lists (Aurora) chemblIDs
-python src/extract_chembl_id.py --compound_list_csv data/atc_table_who_20211101_filtered.csv --output_file_name data/compounds_atc_who_chemblid.json
+file_new=/group/iorio/lucia/cMAP_shared/list_compounds_cMAP.csv
+python src/extract_chembl_id.py --compound_list_csv ${file_new} --output_file_name data/compounds_cMAP_chemblid
+
+# extract ATC compounds chemblIDs
+file_atc=/home/lucia.trastulla/datasets/WHO_ATC_compounds/atc_table_who_20211101_filtered.csv
+python src/extract_chembl_id.py --compound_list_csv ${file_atc} --output_file_name data/compounds_atc_who_chemblid
+
+echo "chembl ID extracted"
 
 # combine lists
 Rscript src/assign_atc_to_compounds_by_chembl.R \
-	--compounds_json
-	--atc_compounds_json
-	--atc_csv
-	--out
+	--compounds_json data/compounds_cMAP_chemblid.json \
+	--atc_compounds_json data/compounds_atc_who_chemblid.json \
+	--atc_csv ${file_atc} \
+	--out data/compounds_cMAP_with_atc
 
+echo "ATC assigned to compound list"
 
